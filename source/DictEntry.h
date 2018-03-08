a59f8a4749ea0d13a8bf3994e0e2f5ff02dc68ae
happyklans <benjamin.stanton@ucdenver.edu>
2018-03-08 04:45:52 +0000

    added the given header and renamed the .h to a .cpp

--- source/DictEntry.h
+++ source/DictEntry.h
@@ -1,7 +1,22 @@
-/* 
-Benjamin Stanton
-CSCI 2421
-3/7/18
+#ifndef DICTENTRY_
+#define DICTENTRY_
+#include <string>
 
-Description: Declarations for the DictEntry class
-*/
+typedef std::string wordType;
+class DictEntry
+{
+private:
+    wordType word;
+    wordType definition;
+    
+public:
+    wordType getWord(){return word;}
+    void setWord(wordType _word){word = _word;}
+    wordType getDef(){return definition;}
+    void setDef(wordType _def){definition = _def;}
+    bool operator < (const DictEntry &entry) const {
+        return (word < entry.word);}
+
+};
+
+#endif
