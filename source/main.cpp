17ebcfbe1208b7a92820afbeb02e804e2411d9aa
happyklans <benjamin.stanton@ucdenver.edu>
2018-03-09 04:01:41 +0000

    changed the name to main

---
+++ source/main.cpp
@@ -0,0 +1,20 @@
+/* 
+Benjamin Stanton
+CSCI 2421
+3/7/18
+
+Description: main program, contains the ui, instantiates the objects and 
+calls the member functions
+*/
+
+#include <list>
+#include <iostream>
+#include "DictEntry.h"
+
+using namespace std;
+
+int main()
+{
+	list<DictEntry> dictionary;
+	
+	
